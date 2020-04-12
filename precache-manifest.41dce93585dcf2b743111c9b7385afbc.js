46a59e3ac95a10bf8338e4ab7f0d628fae9e4b80
Qian <qian.ma3@dcmail.ca>
2020-04-12 15:22:36 -0400

    Updates

---
+++ precache-manifest.41dce93585dcf2b743111c9b7385afbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5a2a750f30201556d6e5a80623b1236",
+    "url": "/Appointment_App/index.html"
+  },
+  {
+    "revision": "6d0ba0ab096fe75755a6",
+    "url": "/Appointment_App/static/css/main.c3879253.chunk.css"
+  },
+  {
+    "revision": "d7140f34fbe12f5b4e67",
+    "url": "/Appointment_App/static/js/2.a467846f.chunk.js"
+  },
+  {
+    "revision": "3cd4b6bc4f51e64816d8a2b69bba1790",
+    "url": "/Appointment_App/static/js/2.a467846f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d0ba0ab096fe75755a6",
+    "url": "/Appointment_App/static/js/main.918efa9f.chunk.js"
+  },
+  {
+    "revision": "3dfc2bcc9434ff63726d",
+    "url": "/Appointment_App/static/js/runtime-main.1aa3fddc.js"
+  },
+  {
+    "revision": "164a20789222851620bfa9ef1dd07a1d",
+    "url": "/Appointment_App/static/media/calendar.164a2078.svg"
+  }
+]);
